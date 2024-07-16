c864a9844f4c7522adf1f7f41db6eed3add0c06d
Johan Linder <johan@fmca.se>
2024-07-16 11:45:55 +0200

    2023 day 3 part 2

--- 2023/day-03/main.go
+++ 2023/day-03/main.go
@@ -71,10 +71,9 @@ func part1(lines []string) int {
 	return sum
 }
 
-func part2(lines []string) int {
-	return 0
-}
-
+/*
+* Return x if it's positive, otherwise 0.
+ */
 func MinZero(x int) int {
 	if x < 0 {
 		return 0
@@ -82,9 +81,78 @@ func MinZero(x int) int {
 	return x
 }
 
+/**
+* Return the smallest integer
+ */
 func MinInt(x, y int) int {
 	if x <= y {
 		return x
 	}
 	return y
+}
+
+/**
+* Part 2
+ */
+func part2(lines []string) int {
+	sum := 0
+	symbRe := regexp.MustCompile(`\*`)
+	for i, line := range lines {
+		matches := symbRe.FindAllStringIndex(line, -1)
+		for _, match := range matches {
+			adjacent := findAdjacentNums(i, match[0], lines)
+			if len(adjacent) == 2 {
+				sum += stringToInt(adjacent[0]) * stringToInt(adjacent[1])
+			}
+		}
+	}
+	return sum
+}
+
+/*
+* Find adjacent (left/right/diagonal) numbers to index.
+ */
+func findAdjacentNums(lineIndex int, index int, lines []string) []string {
+	currentLine := lines[lineIndex]
+	prevLine := ""
+	nextLine := ""
+	adjacent := []string{}
+
+	if lineIndex > 0 {
+		prevLine = lines[lineIndex-1]
+	}
+
+	if lineIndex < len(lines) {
+		nextLine = lines[lineIndex+1]
+	}
+
+	adjacent = append(adjacent, findNumsAtIndex(prevLine, index, 1)...)
+	adjacent = append(adjacent, findNumsAtIndex(currentLine, index, 1)...)
+	adjacent = append(adjacent, findNumsAtIndex(nextLine, index, 1)...)
+
+	return adjacent
+}
+
+/**
+* Find numbers in line that is covering index + padding
+ */
+func findNumsAtIndex(line string, index int, padding int) []string {
+	numRe := regexp.MustCompile(`\d+`)
+	matches := numRe.FindAllStringIndex(line, -1)
+	nums := []string{}
+	for _, match := range matches {
+		if index >= match[0]-padding && index < match[1]+padding {
+			nums = append(nums, line[match[0]:match[1]])
+		}
+	}
+
+	return nums
+}
+
+func stringToInt(s string) int {
+	i, err := strconv.Atoi(s)
+	if err != nil {
+		panic(err)
+	}
+	return i
 }
--- 2023/day-03/main_test.go
+++ 2023/day-03/main_test.go
@@ -25,3 +25,25 @@ func TestPart1(t *testing.T) {
 		t.Fatalf("part1() = %v, want %v", result, want)
 	}
 }
+
+func TestPart2(t *testing.T) {
+	lines := []string{
+		"467..114..",
+		"...*......",
+		"..35..633.",
+		"......#...",
+		"617*......",
+		".....+.58.",
+		"..592.....",
+		"......755.",
+		"...$.*....",
+		".664.598..",
+	}
+
+	want := 467835
+	result := part2(lines)
+
+	if result != want {
+		t.Fatalf("part2() = %v, want %v", result, want)
+	}
+}
