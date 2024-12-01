c148dce08b225781f677154f7751efcea0fbc605
Johan Linder <johan@fmca.se>
2024-12-01 23:11:49 +0100

    2024 day 1 part 2

--- 2024/day-01/main.go
+++ 2024/day-01/main.go
@@ -11,6 +11,7 @@ import (
 func main() {
 	lines := util.ReadFile("input.txt")
 	fmt.Printf("Part 1: %v \n", part1(lines))
+	fmt.Printf("Part 2: %v \n", part2(lines))
 }
 
 func part1(lines []string) int {
@@ -33,6 +34,33 @@ func part1(lines []string) int {
 	for i := 0; i < len(left); i++ {
 		diff := left[i] - right[i]
 		sum += max(diff, -diff)
+	}
+
+	return sum
+}
+
+func part2(lines []string) int {
+	sum := 0
+
+	var left []int
+	var right []int
+
+	for _, l := range lines {
+		split := strings.Fields(l)
+		l, _ := strconv.Atoi(split[0])
+		r, _ := strconv.Atoi(split[1])
+		left = append(left, l)
+		right = append(right, r)
+	}
+
+	for _, le := range left {
+		occ := 0
+		for _, ri := range right {
+			if ri == le {
+				occ++
+			}
+		}
+		sum += le * occ
 	}
 
 	return sum
--- 2024/day-01/main_test.go
+++ 2024/day-01/main_test.go
@@ -21,3 +21,21 @@ func TestPart1(t *testing.T) {
 		t.Fatalf("part1() = %v, want %v", result, want)
 	}
 }
+
+func TestPart2(t *testing.T) {
+	lines := []string{
+		"3   4",
+		"4   3",
+		"2   5",
+		"1   3",
+		"3   9",
+		"3   3",
+	}
+
+	want := 31
+	result := part2(lines)
+
+	if result != want {
+		t.Fatalf("part1() = %v, want %v", result, want)
+	}
+}
